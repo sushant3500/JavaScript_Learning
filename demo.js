2ed505bf82249ed972a2edd6578a5041d9c1e08d
sushant3500 <sushantsawant3500@gmail.com>
2022-10-04 19:17:00 +0530

    First program execution in JS
    First program execution in JS

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
 // This is my first program
 console.log("Hello");
-console.log("My name is Sushant Sawant");
-console.info("This is Info");//Info means Information
-console.warn("I am not bad guy");//warn means warning
-console.error("Opps failed to execute");
+console.log("My name is Sushant Shankar Sawant");
+console.log("traveling,Playing Chess & eating new types of foods");//these are my hobbies
+console.info("Love of Learning,Adaptable and Polite");//these are my personal strength
+console.warn("Shyness,lack of Patience, fear to public speaking");//these are my weakness
+console.log(30 + 50);
+console.log(49 * 479);
