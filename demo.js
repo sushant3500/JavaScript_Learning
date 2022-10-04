212b57a97f4f8c105eee40da88eaf30713f147f7
sushant3500 <sushantsawant3500@gmail.com>
2022-10-04 08:36:45 +0530

    console updated
    console updated

---
+++ demo.js
@@ -0,0 +1,6 @@
+// This is my first program
+console.log("Hello");
+console.log("My name is Sushant Sawant");
+console.info("This is Info");//Info means Information
+console.warn("I am not bad guy");//warn means warning
+console.error("Opps failed to execute");
