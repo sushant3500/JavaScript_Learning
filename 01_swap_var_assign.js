34d011df0568be1e6376c768d81ec55456fe3399
sushant3500 <sushantsawant3500@gmail.com>
2022-10-06 18:35:06 +0530

    variable and swapping updated
    variable and swapping updated

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,26 @@
+console.log("****Assignment No-02****");
+console.log("----Q 2.1----");
+var sweety = "sweety";
+var cutie = "cutie";
+console.log("Before swapping");
+console.log(sweety, cutie);
+
+var swapped_name = sweety;
+sweety = cutie;
+cutie=swapped_name;
+console.log("After swapping");
+console.log(sweety, cutie);
+
+console.log("----Q 2.2----");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("Before swapping number");
+console.log(num1, num2, num3);
+
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("After swapping number");
+console.log(num1, num2, num3);
---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+console.log("---Assignment No-01---");
+console.log("**Q 1.1**");
+var myFullName;
+console.log(myFullName);
+myFullName = "Sushant Sawant";
+console.log(myFullName);
+myFullName = "Sushant Shankar Sawant";
+console.log(myFullName);
+
+console.log("**Q 1.2**");
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+
+console.log("**Q 1.3**");
+
+var firstName = "Sushant", lastName = "Sawant", age = "25", address = "Sangli", gender = "Male", is_married = "No";
+
+console.log("firstName:-"+firstName, "lastName:-"+lastName, "age:-" +age, "address:-" +address, "is_married:-" +is_married);
+//-------------------------------------------------------------------
