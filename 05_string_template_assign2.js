47918a4e74d559d1645cd928040a633acaf282cd
sushant3500 <sushantsawant3500@gmail.com>
2022-10-11 21:17:25 +0530

    updated string template
    updated string template

--- 05_string_template_assign2.js
+++ 05_string_template_assign2.js
@@ -1,33 +1,49 @@
-console.log(`=========Assignment No-02============`);
-var imp = function stringHandsOn() {
-    var string = "  Hey you are doing good, keep it up  ";
-    console.log('1. Print of String:-', string);
-    var LengthOfString = string.length;
-    console.log("---------------------------------------------");
-    console.log(`2. Length of given string is:`, LengthOfString);
-    console.log("---------------------------------------------");
-    var space = string.trim();
-    console.log('3.Removing spaces:-', space);
-    console.log("---------------------------------------------");
-    var count = space.length;
-    var sush = LengthOfString - count;
-    console.log("4.count spaces:-", sush);
-    console.log("---------------------------------------------");
-    console.log("5.first character:-", space.charAt(0), "last character:-", space.charAt(count - 1));
-    console.log("---------------------------------------------");
-    var noOfWords = space.split(" ");
-    console.log("6.Total no.of words:-", noOfWords.length);
-    console.log("---------------------------------------------");
-    console.log(`7.Index no. of "good":-`, string.indexOf("good"));
-    console.log("---------------------------------------------");
-    console.log(`8.1.substring from index 22:-`, string.substring(22));
-    console.log(`8.2.slice from index 22:-`, string.slice(22));
-    console.log("---------------------------------------------");
-    console.log(`9.string end with "up":-`, string.includes("up"));
-    console.log("---------------------------------------------");
-    console.log(`10.string start with "Hey":-`, space.includes("Hey"));
-    console.log("---------------------------------------------");
+
+console.log("=============================Assignment No-02===============================");
+var stringHandsOn = function () {
+    var string = "   Hey you are doing good, keep it up   ";
+    console.log(`1.String before performing any operation:- ${string}`);
+
+    console.log("============================================================");
+    console.log(`2.length of sting:${string.length}`);
+
+    console.log("============================================================");
+    var trimResult = string.trim();
+    console.log(`3.After removing spaces:-${trimResult}`);
+
+
+    console.log("============================================================");
+    var count = (string.length - trimResult.length);
+    console.log(`4.count space =${count}`);
+
+    console.log("============================================================");
+    var character = trimResult.charAt(0);
+    var charResult = trimResult.charAt(trimResult.length - 1);
+    console.log(`5.first character:${character}   And last character: ${charResult}`);
+
+    console.log("============================================================");
+    var spaceCount = trimResult.split(" ");
+    console.log(`6.total words count= ${spaceCount.length}`);
+
+    console.log("============================================================");
+    var position = string.indexOf("good");
+    console.log(`7.indexof word "good" =${position}`);
+
+    console.log("============================================================");
+    console.log(`8.1 substring from 22= ${string.substring(22)}`);
+
+    console.log(`8.2 slice from 22=${string.slice(22)}`);
+
+    console.log("============================================================");
+    console.log(`9.string end with up:${trimResult.endsWith("up")}`);
+    console.log("============================================================");
+    console.log(`10.string start with Hey:${trimResult.startsWith("Hey")}`);
+    console.log("============================================================");
 }
-imp();
+stringHandsOn();
+
+
+
+
 
 
