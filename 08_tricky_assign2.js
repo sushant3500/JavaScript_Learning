7d70019c2add7d4f0bd30b21509e37ecb3e73fea
sushant3500 <sushantsawant3500@gmail.com>
2022-10-18 17:34:05 +0530

    spaces removed from string
    spaces removed from string

--- 08_tricky_assign2.js
+++ 08_tricky_assign2.js
@@ -1,19 +1,20 @@
 console.log("==========Assignment No-0B===========");
 function reverseString(str) {
-    var reverse = "";
-    var stringGiven = str;
-    var totalString = stringGiven.length;
-    console.log(`Given String is:- "${str}"`);
-    console.log("*************After Reverse the String************");
-    for (let index = totalString; index >= 0; index--) {
-      var charAt = stringGiven.charAt(index);
+  var reverse = "";
+  var stringGiven = str;
+  var totalString = stringGiven.length;
+  console.log(`Given String is:- "${str}"`);
+  console.log("----------------After Reverse the String----------------");
+  for (let index = totalString; index >= 0; index--) {
+    var charAt = stringGiven.charAt(index);
+    if (charAt != " ") {
       reverse = reverse + charAt
-  
     }
-    console.log(`Reverse String Output:- "${reverse}"`);
-    console.log("**************************************************");
-  
+
   }
-  reverseString("Hard work always pays back");
-  reverseString("Soon I will be Angular IT Champ");
-  
+  console.log(`Reverse String Output:- "${reverse}"`);
+  console.log("----------------------------------------------------------");
+
+}
+reverseString("Hard work always pays back");
+reverseString("Soon I will be Angular IT Champ");
